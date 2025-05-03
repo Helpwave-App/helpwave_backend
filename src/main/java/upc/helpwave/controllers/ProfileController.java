18cf0df775bd89bafec5d50b4900f2d41b0d9d79
Elvi98 <elvia.arteaga98@hotmail.com>
2025-05-03 13:39:28 -0500

    Added extra filter

--- src/main/java/upc/helpwave/controllers/ProfileController.java
+++ src/main/java/upc/helpwave/controllers/ProfileController.java
@@ -15,35 +15,38 @@ import java.util.stream.Collectors;
 public class ProfileController {
     @Autowired
     private IProfileService pS;
+
     @PostMapping
-    public void register(@RequestBody ProfileDTO dto){
-        ModelMapper m=new ModelMapper();
-        Profile r=m.map(dto, Profile.class);
+    public void register(@RequestBody ProfileDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Profile r = m.map(dto, Profile.class);
         pS.insert(r);
     }
+
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id)
-    {
+    public void delete(@PathVariable("id") Integer id) {
         pS.delete(id);
     }
 
     @GetMapping("/{id}")
-    public ProfileDTO listId(@PathVariable("id")Integer id){
-        ModelMapper m=new ModelMapper();
-        ProfileDTO dto=m.map(pS.listId(id),ProfileDTO.class);
+    public ProfileDTO listId(@PathVariable("id") Integer id) {
+        ModelMapper m = new ModelMapper();
+        ProfileDTO dto = m.map(pS.listId(id), ProfileDTO.class);
         return dto;
     }
+
     @GetMapping
-    public List<ProfileDTO> list(){
-        return pS.list().stream().map(x->{
-            ModelMapper m=new ModelMapper();
-            return m.map(x,ProfileDTO.class);
+    public List<ProfileDTO> list() {
+        return pS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, ProfileDTO.class);
         }).collect(Collectors.toList());
     }
+
     @PutMapping
     public void update(@RequestBody ProfileDTO dto) {
         ModelMapper m = new ModelMapper();
-        Profile a=m.map(dto,Profile.class);
+        Profile a = m.map(dto, Profile.class);
         pS.insert(a);
     }
 }
