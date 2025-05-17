40e5a4d8efe6838fe602d906059b6589fc778350
u202116806 <u202116806@upc.edu.pe>
2025-05-16 19:38:31 -0500

    16-05-2025 / Sprint 2 Notifications and Empairing

--- src/main/java/upc/helpwave/repositories/VideocallRepository.java
+++ src/main/java/upc/helpwave/repositories/VideocallRepository.java
@@ -1,6 +1,8 @@
 package upc.helpwave.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import upc.helpwave.entities.Empairing;
 import upc.helpwave.entities.Videocall;
@@ -11,4 +13,8 @@ import java.util.Optional;
 @Repository
 public interface VideocallRepository extends JpaRepository<Videocall, Integer> {
     Optional<Videocall> findByEmpairing(Empairing empairing);
+    @Query("SELECT v FROM Videocall v " +
+            "WHERE v.empairing.profile.idProfile = :idProfile " +
+            "AND v.endVideocall > CURRENT_TIMESTAMP")
+    List<Videocall> findActiveVideocallsByProfile(@Param("idProfile") int idProfile);
 }
