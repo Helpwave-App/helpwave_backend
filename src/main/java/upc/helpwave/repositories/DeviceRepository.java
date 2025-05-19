ebf5cc0c4396d17abd031b4a112d238eaaed23e4
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-05-19 13:45:54 -0500

    Update DeviceRepository.java

--- src/main/java/upc/helpwave/repositories/DeviceRepository.java
+++ src/main/java/upc/helpwave/repositories/DeviceRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import upc.helpwave.entities.Device;
 
+import java.util.Optional;
+
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Integer>{
+    void deleteByTokenDevice(String tokenDevice);
+    Optional<Device> findByTokenDevice(String tokenDevice);
 }
