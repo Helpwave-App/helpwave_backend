f3e13f1c73c4266f77864d090154a201049cab15
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-05-20 15:10:31 -0500

    Update NotificationMessageDTO.java

--- src/main/java/upc/helpwave/dtos/NotificationMessageDTO.java
+++ src/main/java/upc/helpwave/dtos/NotificationMessageDTO.java
@@ -11,4 +11,44 @@ public class NotificationMessageDTO {
     private String body;
     private String image;
     private Map<String, String> data;
+
+    public String getTokenDevice() {
+        return tokenDevice;
+    }
+
+    public void setTokenDevice(String tokenDevice) {
+        this.tokenDevice = tokenDevice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Map<String, String> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, String> data) {
+        this.data = data;
+    }
 }
