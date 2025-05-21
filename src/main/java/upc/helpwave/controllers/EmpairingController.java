7655cffb393ab151cbf19dc9b744762c3c19ceff
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-05-20 19:41:06 -0500

    Update EmpairingController.java

--- src/main/java/upc/helpwave/controllers/EmpairingController.java
+++ src/main/java/upc/helpwave/controllers/EmpairingController.java
@@ -5,14 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import upc.helpwave.dtos.EmpairingDTO;
+import upc.helpwave.dtos.NotificationMessageDTO;
 import upc.helpwave.dtos.VideocallDTO;
 import upc.helpwave.entities.Empairing;
+import upc.helpwave.entities.Request;
+import upc.helpwave.entities.User;
 import upc.helpwave.entities.Videocall;
 import upc.helpwave.serviceimplements.EmpairingServiceImplement;
+import upc.helpwave.serviceimplements.FirebaseMessagingServiceImplement;
 import upc.helpwave.serviceinterfaces.IEmpairingService;
+import upc.helpwave.serviceinterfaces.IRequestService;
 import upc.helpwave.serviceinterfaces.IVideocallService;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/empairings")
@@ -23,6 +29,10 @@ public class EmpairingController {
     private IVideocallService vS;
     @Autowired
     private EmpairingServiceImplement eSI;
+    @Autowired
+    private FirebaseMessagingServiceImplement fMS;
+    @Autowired
+    private IRequestService rS;
     @PostMapping
     public void register(@RequestBody EmpairingDTO dto){
         ModelMapper m=new ModelMapper();
@@ -58,6 +68,28 @@ public class EmpairingController {
     public ResponseEntity<VideocallDTO> acceptEmpairing(@PathVariable int empairingId) {
         Videocall videocall = eSI.acceptEmpairing(empairingId);
         if (videocall != null) {
+            Empairing empairing = eS.listId(empairingId);
+            Request request = empairing.getRequest();
+            User user = request.getProfile().getUser();
+
+            String tokenDevice = null;
+            if (user != null && user.getDevices() != null && !user.getDevices().isEmpty()) {
+                tokenDevice = user.getDevices().get(0).getTokenDevice();
+            }
+
+            if (tokenDevice != null && !tokenDevice.isEmpty()) {
+                NotificationMessageDTO message = new NotificationMessageDTO();
+                message.setTokenDevice(tokenDevice);
+                message.setTitle("Tu solicitud ha sido aceptada");
+                message.setBody("Estás entrando a una videollamada con un voluntario.");
+                message.setImage(null);
+                message.setData(Map.of(
+                        "type", "videocall_start",
+                        "channel", videocall.getChannel(),
+                        "token", videocall.getToken()));
+                fMS.sendNotificationByToken(message);
+            }
+
             VideocallDTO dto = new VideocallDTO(videocall.getToken(), videocall.getChannel());
             return ResponseEntity.ok(dto);
         } else {
