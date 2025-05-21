384b691dbb99be01def47bc3b88daa8ea22a7642
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-05-20 19:52:42 -0500

    Update IFirebaseMessagingService.java

--- src/main/java/upc/helpwave/serviceinterfaces/IFirebaseMessagingService.java
+++ src/main/java/upc/helpwave/serviceinterfaces/IFirebaseMessagingService.java
@@ -3,7 +3,6 @@ package upc.helpwave.serviceinterfaces;
 import upc.helpwave.dtos.NotificationMessageDTO;
 
 public interface IFirebaseMessagingService {
-
     public String sendNotificationByToken(NotificationMessageDTO dto);
-
+    public String sendSilentNotificationByToken(NotificationMessageDTO dto);
 }
