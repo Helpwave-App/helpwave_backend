581e614fc95a361479ab9a38e89facfb927c37f2
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-05-29 18:11:13 -0500

    Update CommentsDTO.java

--- src/main/java/upc/helpwave/dtos/CommentsDTO.java
+++ src/main/java/upc/helpwave/dtos/CommentsDTO.java
@@ -1,42 +1,99 @@
 package upc.helpwave.dtos;
 
-public class CommentsDTO {
-    private int idVideocall;
-    private String descriptionComment;
-    private int scoreVolunteer;
-    private int scoreVideocall;
+import upc.helpwave.entities.Level;
 
-    public int getIdVideocall() {
-        return idVideocall;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ProfileDTO {
+    private int idProfile;
+    private Level level;
+    private String name;
+    private String lastName;
+    private LocalDate birthDate;
+    private BigDecimal scoreProfile;
+    private String email;
+    private String phoneNumber;
+    private String photoUrl;
+    private Integer assistances;
+
+    public Integer getAssistances() {
+        return assistances;
     }
 
-    public void setIdVideocall(int idVideocall) {
-        this.idVideocall = idVideocall;
+    public void setAssistances(Integer assistances) {
+        this.assistances = assistances;
     }
 
-    public String getDescriptionComment() {
-        return descriptionComment;
+    public int getIdProfile() {
+        return idProfile;
     }
 
-    public void setDescriptionComment(String descriptionComment) {
-        this.descriptionComment = descriptionComment;
+    public void setIdProfile(int idProfile) {
+        this.idProfile = idProfile;
     }
 
-    public int getScoreVolunteer() {
-        return scoreVolunteer;
+    public Level getLevel() {
+        return level;
     }
 
-    public void setScoreVolunteer(int scoreVolunteer) {
-        this.scoreVolunteer = scoreVolunteer;
+    public void setLevel(Level level) {
+        this.level = level;
     }
 
-    public int getScoreVideocall() {
-        return scoreVideocall;
+    public String getName() {
+        return name;
     }
 
-    public void setScoreVideocall(int scoreVideocall) {
-        this.scoreVideocall = scoreVideocall;
+    public void setName(String name) {
+        this.name = name;
     }
-}
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public BigDecimal getScoreProfile() {
+        return scoreProfile;
+    }
+
+    public void setScoreProfile(BigDecimal scoreProfile) {
+        this.scoreProfile = scoreProfile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
     }
 }
