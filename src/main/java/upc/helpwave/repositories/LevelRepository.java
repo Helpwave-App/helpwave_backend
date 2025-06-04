1a3240b92ab9f2d2ef71fa505309d1c461f756c4
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-06-03 22:51:30 -0500

    Update LevelRepository.java

--- src/main/java/upc/helpwave/repositories/LevelRepository.java
+++ src/main/java/upc/helpwave/repositories/LevelRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import upc.helpwave.entities.Level;
 
+import java.util.Optional;
+
 @Repository
 public interface LevelRepository extends JpaRepository<Level, Integer> {
+    Optional<Level> findFirstByMinRequestGreaterThanOrderByMinRequestAsc(Integer assistances);
 }
