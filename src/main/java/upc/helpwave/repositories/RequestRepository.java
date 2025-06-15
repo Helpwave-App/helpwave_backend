6766b4a927442aaa2150d70eb7df5caf7d1d5fb7
fabricuchca <132107507+fabricuchca@users.noreply.github.com>
2025-06-15 11:54:50 -0500

    Update RequestRepository.java

--- src/main/java/upc/helpwave/repositories/RequestRepository.java
+++ src/main/java/upc/helpwave/repositories/RequestRepository.java
@@ -14,5 +14,7 @@ public interface RequestRepository extends JpaRepository<Request, Integer> {
     @Query("SELECT r FROM Request r WHERE r.profile.idProfile = :idProfile AND r.dateRequest > :threshold")
     List<Request> findRecentByProfile(@Param("idProfile") int idProfile, @Param("threshold") LocalDateTime threshold);
     List<Request> findByStateRequestTrueAndDateRequestBefore(LocalDateTime threshold);
-
+    @Query("SELECT r FROM Request r WHERE r.stateRequest = true AND r.dateRequest < :threshold AND NOT EXISTS " +
+            "(SELECT e FROM Empairing e WHERE e.request = r AND e.stateEmpairing = true)")
+    List<Request> findTrulyPendingRequestsOlderThan(@Param("threshold") LocalDateTime threshold);
 }
